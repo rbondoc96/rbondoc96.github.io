f4771e408c8cdbc6e978325307e274ca0714259f
Rodrigo Bondoc <rbondoc96@gmail.com>
2022-10-27 14:38:24 -0700

    update: saving old logo

---
+++ src/assets/icons/Logo.tsx
@@ -0,0 +1,12 @@
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span className="font-bold text-4xl text-blue" color="primary.main">
+        rb
+      </span>
+      <span className="text-black dark:text-white text-4xl">ondoc</span>
+    </div>
+  );
+};
+
+export default Logo;
