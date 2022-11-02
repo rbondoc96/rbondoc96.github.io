5984b6e9fc7655e70fa2ed35996c9841ae8e805b
Rodrigo Bondoc <rbondoc96@gmail.com>
2022-11-01 20:53:51 -0700

    update: added dark mode and new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,37 @@
+const colors = require('tailwindcss/colors');
+const {fontSizes} = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      black: 'rgb(9, 9, 20)',
+      dark: 'hsl(200, 3%, 21%)',
+      light: 'hsl(0, 0%, 95%)',
+      white: 'rgb(243, 243, 243)',
+      orange: 'rgb(222, 150, 80)',
+      blue: {
+        DEFAULT: 'rgb(90, 180, 231)',
+      },
+      gray: {
+        DEFAULT: colors.gray['300'],
+      },
+      overlay: {
+        DEFAULT: 'rgba(20, 20, 20, 0.5)',
+      },
+      primary: {
+        DEFAULT: 'hsl(347, 67.8%, 47.5%)',
+      },
+      secondary: {
+        DEFAULT: 'hsl(185, 90.2%, 48.2%)',
+      },
+    },
+    fontSize: {
+      xl: 'clamp(1rem, 2.5vw, 1.25rem)',
+      '7xl': 'clamp(3rem, 10vw, 4.5rem)',
+    },
     screens: {
       sm: '640px',
       md: '768px',
