0ef2c6cba82f5fac6c8ef581c63b047ac3579050
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-03-14 02:36:04 -0700

    update: home hero section for web/mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,20 @@
 import gsap from 'gsap';
 import type {Component} from 'solid-js';
 import {createEffect} from 'solid-js';
+// import Typed from 'typed.js';
 
 import Navigation from '@/components/Navigation';
-import SectionHeader from '@/components/SectionHeader';
 import Socials from '@/components/Socials';
 
-import './index.css';
+import '@/styles/home.css';
 
 const Home: Component = () => {
     createEffect(() => {
+        // const typed = new Typed('#home-about-typed', {
+        //     strings: ['Rodrigo Bondoc', 'My Name'],
+        //     typeSpeed: 30,
+        // });
+
         const timeline = gsap.timeline();
         timeline
             .from('.js-nav', {
@@ -27,7 +32,7 @@ const Home: Component = () => {
                 '<',
             )
             .from(
-                '.js-title',
+                '.js-hero-content',
                 {
                     opacity: 0,
                     translateX: 200,
@@ -38,6 +43,24 @@ const Home: Component = () => {
             );
     });
 
+    const scrollTo = (event: Event) => {
+        event.preventDefault();
+
+        const target = event.currentTarget as HTMLAnchorElement;
+        const href = target.getAttribute('href');
+
+        if (href === null) {
+            return;
+        }
+        const element = document.querySelector(href);
+        if (element !== null) {
+            setTimeout(() => {
+                element.scrollIntoView({behavior: 'smooth'});
+                target.blur();
+            }, 150);
+        }
+    };
+
     return (
         <>
             <header>
@@ -45,40 +68,74 @@ const Home: Component = () => {
             </header>
 
             <main class="home flex items-center">
-                <section id="home-welcome">
-                    <div class="flex-1 flex items-center">
-                        <div class="js-title flex flex-col">
-                            <h1 class="text-white text-7xl font-bold">
-                                Rodrigo Bondoc
-                            </h1>
-                            <h2 class="text-white text-2xl font-light">
-                                Full Stack Software Engineer
-                            </h2>
+                <section id="home-welcome" class="home-section">
+                    <div class="hero">
+                        <div class="js-hero-content hero-content">
+                            <div class="hero-content-header">
+                                <div class="hero-preamble">
+                                    Hey there. My name is
+                                </div>
+                                <h1 class="hero-title">Rodrigo Bondoc</h1>
+                                <h3 class="hero-subtitle">
+                                    Full Stack Software Developer
+                                </h3>
+                            </div>
+                            <p class="hero-body">
+                                <span class="hero-caption">
+                                    I'm a web and mobile app developer with a
+                                    background in electrical engineering.
+                                </span>
+                                <span class="hero-caption">
+                                    In other words, I joined the dark (soft)
+                                    side.
+                                </span>
+                            </p>
+                            <div>
+                                <a
+                                    class="hero-link hero-link--joke"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    href="https://knowyourmeme.com/memes/come-to-the-dark-side"
+                                >
+                                    Yes, they did have cookies.
+                                </a>
+                            </div>
+                            <div>
+                                <a
+                                    class="hero-link"
+                                    href="#home-about"
+                                    onClick={scrollTo}
+                                >
+                                    <button type="button" class="hero-button">
+                                        Get to know me!
+                                    </button>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </section>
 
-                <section id="home-about">
+                <section id="home-about" class="home-section">
                     <div class="flex-1 flex flex-col">
-                        <SectionHeader>Me, Myself, and I</SectionHeader>
+                        <h2 class="section-title">Me, Myself, and I</h2>
                     </div>
                 </section>
 
-                <section id="home-experience">
+                <section id="home-experience" class="home-section">
                     <div class="flex-1 flex flex-col">
-                        <SectionHeader>My Experience</SectionHeader>
-                    </div>
-                </section>
-                
-                <section id="home-projects">
-                    <div class="flex-1 flex flex-col">
-                        <SectionHeader>Things I&apos;ve Built</SectionHeader>
+                        <h2 class="section-title">My Experience</h2>
                     </div>
                 </section>
 
-                <section id="home-contact">
+                <section id="home-projects" class="home-section">
                     <div class="flex-1 flex flex-col">
-                        <SectionHeader>Let&apos;s get in touch!</SectionHeader>
+                        <h2 class="section-title">Things I&apos;ve Built</h2>
+                    </div>
+                </section>
+
+                <section id="home-contact" class="home-section">
+                    <div class="flex-1 flex flex-col">
+                        <h2 class="section-title">Let&apos;s get in touch!</h2>
                     </div>
                 </section>
             </main>
