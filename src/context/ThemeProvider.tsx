b655ab2cc404344c6d669db3fdb32476a67e39b9
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-03-16 19:30:22 -0700

    update: add global style resets to ThemeProvider

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,10 +1,30 @@
 import type {ParentComponent} from 'solid-js';
-import {ThemeProvider as StyledThemeProvider} from 'solid-styled-components';
+import {
+    createGlobalStyles,
+    ThemeProvider as StyledThemeProvider,
+} from 'solid-styled-components';
 
 import theme from '@/theme';
 
+const GlobalStyles = createGlobalStyles`
+    html {
+        font-family: 'Work Sans', sans-serif;
+        scroll-behavior: smooth;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: ${theme.colors.dark};
+    }
+`;
+
 const ThemeProvider: ParentComponent = (props) => (
-    <StyledThemeProvider theme={theme}>{props.children}</StyledThemeProvider>
+    <StyledThemeProvider theme={theme}>
+        <GlobalStyles />
+
+        {props.children}
+    </StyledThemeProvider>
 );
 
 export default ThemeProvider;
