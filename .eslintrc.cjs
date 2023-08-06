5eeb2efcc398bc9ad952f7b8278bc2fb2212804c
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-08-06 04:30:22 -0700

    chore: update @pxeeio/eslint-config and .eslintrc config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,31 +3,5 @@
 const {defineConfig} = require('eslint-define-config');
 
 module.exports = defineConfig({
-    env: {
-        browser: true,
-        es2020: true,
-    },
     extends: '@pxeeio/eslint-config-solid',
-    plugins: ['@typescript-eslint', 'import', 'simple-import-sort', 'solid'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
-    settings: {
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
-    },
-    rules: {
-        'import/named': 'off',
-        'import/no-named-as-default': 'off',
-        'import/no-unresolved': 'error',
-        'object-curly-spacing': ['error', 'never'],
-        'require-jsdoc': 'off',
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/consistent-type-definitions': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-    },
-    overrides: [],
 });
